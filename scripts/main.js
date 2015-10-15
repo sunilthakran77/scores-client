d17fbef51c00de3bf29b03d244d77ae54c9b166c
sunilthakran77 <sunilthakran77@gmail.com>
2015-10-15 05:18:42 +0000

    added some content

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+//start ajax request
+$.ajax({
+    url: "data.json",
+    //force to handle it as text
+    dataType: "text",
+    success: function(data) {
+
+        //data downloaded so we call parseJSON function 
+        //and pass downloaded data
+        var json = $.parseJSON(data);
+        //now json variable contains data in json format
+        //let's display a few items
+        for (var i=0;i<json.length;++i)
+        {
+            $('#results').append('<div class="name">'+json[i].name+'</>');
+        }
+    }
+});
+	
+	
