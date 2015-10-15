1544868a13fad815f87ae0400a1a36ddc6f24635
sunilthakran77 <sunilthakran77@gmail.com>
2015-10-15 05:36:17 +0000

    added some content

--- scripts/main.js
+++ scripts/main.js
@@ -10,11 +10,34 @@ $.ajax({
         var json = $.parseJSON(data);
         //now json variable contains data in json format
         //let's display a few items
-        for (var i=0;i<json.length;++i)
+        var i;
+        for (i=0;i<json.length;++i)
         {
             $('#results').append('<div class="name">'+json[i].name+'</>');
         }
     }
 });
+
+//second example
+    var teams = $('#teams').val();
+    var games = $('#games').val();
+    var datastring = 'team=' + team + '&game=' + game;
+    $.ajax({
+        type: "GET",
+        cache: "true",
+        url: "WIS",
+        datatype:"json",
+        data: datastring,
+        success: function(data){
+            var main = $('#main'),
+            var i,
+            var j;
+            for (i = 0, j = data.length; i < j; i++) {
+
+                  $("#main").append('<div>' + data[i].teams + '</div>');
+                  $("#main").append('<div>' + data[i].games + '</div>');
+              }
+        }
+    });
 	
 	
